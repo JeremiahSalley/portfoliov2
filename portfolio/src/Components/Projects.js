4349c2d9a79c166311a387ce791570c5fa4a07b7
Jeremiah Salley <Jeremiah.salley@yahoo.com>
2021-05-29 14:31:51 -0400

    finished

--- portfolio/src/Components/Projects.js
+++ portfolio/src/Components/Projects.js
@@ -73,7 +73,7 @@ function Projects() {
                   </span>
                 </button>
               </Link>
-              <Link>
+              <Link href= 'https://nba-graph-98a16.web.app/'>
                 <button>
                   <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
                     Deployed Site
